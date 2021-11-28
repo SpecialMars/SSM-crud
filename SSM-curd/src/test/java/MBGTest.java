6b55a2bee1f8eb247b75fa8c2644695bbfa90c6e
mars <876679241@qq.com>
2021-11-28 15:04:46 +0800

    second commit mbg逆向工程生成bean和mapper

---
+++ SSM-curd/src/test/java/MBGTest.java
@@ -0,0 +1,30 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName:MBGTest
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2021/11/28 14:51
+ * @Author:Mars
+ */
+public class MBGTest {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
