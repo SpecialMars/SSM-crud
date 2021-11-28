500c54a88c937b3e1b6a09c5af4dc30f47add9f0
mars <876679241@qq.com>
2021-11-28 20:46:18 +0800

    test finish

--- SSM-curd/src/test/java/MapperTest.java
+++ SSM-curd/src/test/java/MapperTest.java
@@ -2,6 +2,7 @@ import com.mars.bean.Department;
 import com.mars.bean.Employee;
 import com.mars.dao.DepartmentMapper;
 import com.mars.dao.EmployeeMapper;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +11,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.UUID;
+
 /**
  * ClassName:MapperTest
  * Package:PACKAGE_NAME
  * Description：测试dao层的工作
- *  推荐Spring的项目就可以使用Spring的单元测试，可以自动注入我们需要的组件
- *  步骤
- *      1、导入SpringTest模块
- *      2、@ContextConfiguration注解指定Spring配置文件的位置
- *      3、直接AutoWired要使用的组件即可
+ * 推荐Spring的项目就可以使用Spring的单元测试，可以自动注入我们需要的组件
+ * 步骤
+ * 1、导入SpringTest模块
+ * 2、@ContextConfiguration注解指定Spring配置文件的位置
+ * 3、直接AutoWired要使用的组件即可
  *
  * @Date:2021/11/28 17:22
  * @Author:Mars
@@ -33,8 +36,11 @@ public class MapperTest {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Autowired
+    SqlSession sqlSession;
+
     @Test
-    public void testCRUD(){
+    public void testCRUD() {
         /*
           1、创建SpringIOC容器
           ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -47,7 +53,20 @@ public class MapperTest {
         //departmentMapper.insertSelective(new Department(null,"测试部"));
 
         // 测试2：在emp表中添加员工
-        employeeMapper.insertSelective(new Employee(null,"lisi","1","4646@163.com",3));
-        employeeMapper.insertSelective(new Employee(null,"wangwu","0","45252@163.com",2));
+        // employeeMapper.insertSelective(new Employee(null,"lisi","1","4646@163.com",3));
+        // employeeMapper.insertSelective(new Employee(null,"wangwu","0","45252@163.com",2));
+
+        // 测试3：批量在emp表中添加员工
+        // 这个mapper可以执行批量操作
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        for (int i = 0; i < 1000; i++) {
+            String emp_name = UUID.randomUUID().toString().substring(0, 5) + i;
+            String gender = i % 2 == 0 ? "W" : "M";
+            String email = i % 3 == 0 ? emp_name + "@163.com" : emp_name + "@qq.com";
+            int did = i % 2 == 0 ? 1 : 2;
+            mapper.insertSelective(new Employee(null, emp_name, gender, email, did));
+        }
+        System.out.println("添加完成");
+
     }
 }
