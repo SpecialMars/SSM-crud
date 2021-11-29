200ac082dc4040e2bcb87caa0dd5033ca497ff63
mars <876679241@qq.com>
2021-11-29 17:58:24 +0800

    查询方法返回JSON字符串

--- SSM-curd/src/main/java/com/mars/controller/EmployeeController.java
+++ SSM-curd/src/main/java/com/mars/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.mars.bean.Employee;
+import com.mars.bean.Msg;
 import com.mars.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -28,12 +30,14 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     /**
-     * 查询员工数据（分页查询）
+     * 查找所有用户信息，返回对象为一个JSON字符串
      *
+     * @param pn
      * @return
      */
     @RequestMapping("/emps")
-    public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
+    @ResponseBody
+    public Msg getEmpsWithJSON(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
         // 使用分页助手插件
         // 在查询之前只需要调用，传入页码，和每页的大小（每页查询个数）
         PageHelper.startPage(pn, 5);
@@ -42,8 +46,27 @@ public class EmployeeController {
         // 使用PageInfo包装查询的结果，然后将PageInfo交给页面
         // PageInfo中封装了详细的分页信息，包括查询出来的数据，传入连续显示的页数
         PageInfo page = new PageInfo(employeeList, 5);
-        model.addAttribute("pageInfo", page);
 
-        return "list";
+        return Msg.success().add("pageInfo",page);
     }
+
+    /**
+     * 查询员工数据（分页查询）
+     *
+     * @return
+     */
+//    @RequestMapping("/emps")
+//    public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
+//        // 使用分页助手插件
+//        // 在查询之前只需要调用，传入页码，和每页的大小（每页查询个数）
+//        PageHelper.startPage(pn, 5);
+//        // startPage后紧跟的整个查询就是分页查询
+//        List<Employee> employeeList = employeeService.getAll();
+//        // 使用PageInfo包装查询的结果，然后将PageInfo交给页面
+//        // PageInfo中封装了详细的分页信息，包括查询出来的数据，传入连续显示的页数
+//        PageInfo page = new PageInfo(employeeList, 5);
+//        model.addAttribute("pageInfo", page);
+//
+//        return "list";
+//    }
 }
